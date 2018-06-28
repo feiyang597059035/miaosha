8c3b502d6d2aff72ddc3656a657b7cc355ebb60a
luogang <luog@xinguangnet.com>
2018-06-28 20:40:11 +0800

    ffddf

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
-		int i=2;
+		int i=22;
 }
 
 /*@SpringBootApplication  
